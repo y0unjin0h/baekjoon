fa14e16003ac9ec88b5d7e727c62fbdac75556ce
y0unj!n0h <85925923+y0unjin0h@users.noreply.github.com>
2022-03-29 18:35:19 +0900

    Create 1927.py

---
+++ 1927.py
@@ -0,0 +1,15 @@
+import sys, heapq
+
+heap = []
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    x = int(sys.stdin.readline())
+
+    if x:
+        heapq.heappush(heap, -x) # 최소 힙을 지원하기 때문에 -를 붙여 최대 힙으로 수정
+    else:
+        if len(heap):
+            print(-heapq.heappop(heap)) 
+        else:
+            print(0)      
